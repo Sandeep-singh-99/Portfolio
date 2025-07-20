7532fcd6d0a4ea03ca173ab8b7461012afdb45ea
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-07-20 19:45:02 +0530

    feat: update skill grid layout to improve responsiveness on smaller screens

--- src/app/(main)/skill/page.tsx
+++ src/app/(main)/skill/page.tsx
@@ -19,7 +19,7 @@ export default async function SkillPage() {
         </h1>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-3 mb-8">
+      <div className="grid grid-cols-2 lg:grid-cols-5 gap-3 mb-8">
         {skills.map((skill) => (
           <div
             key={skill.id}
