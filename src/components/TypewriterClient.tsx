8560bb1872a5c179a01619933ee3a2e4d16a068d
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-12-09 22:48:15 +0530

    feat: Add a new client component for a typewriter effect.

--- src/components/TypewriterClient.tsx
+++ src/components/TypewriterClient.tsx
@@ -10,7 +10,7 @@ interface TypewriterClientProps {
 
 export default function TypewriterClient({ words }: TypewriterClientProps) {
   return (
-    <h2 className="md:text-4xl text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-gray-300 via-gray-400 to-gray-600">
+    <h2 className="md:text-3xl text-lg font-bold bg-clip-text text-transparent bg-gradient-to-r from-gray-300 via-gray-400 to-gray-600">
       <Typewriter
         options={{
           loop: true,
