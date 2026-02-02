e93ffbe6509bb264d6547050af787e66f45b9971
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2026-02-02 20:59:42 +0530

    feat: Add NextAuth middleware to protect admin panel routes.

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,9 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
     pages: {
-        signIn: "/admin-panel/login", // redirect here if not authenticated
+        signIn: "/admin-panel/login", 
     },
 });
-
-// export const config = {
-//     matcher: ["/dashboard", "/intro"], // protected routes
-// };
-
 
 export const config = {
     matcher: ["/admin-panel/:path*"], // protect all admin + intro
